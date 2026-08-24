d3637eb146ed5b600d82c6d5215b2f2ffe1d5d96
Sebas2103 <juanito2103@hotmail.es>
2026-08-24 11:24:52 -0500

    Demostracion Andres

--- PHP/Clases/db_conexion.php
+++ PHP/Clases/db_conexion.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
-$dbname = "mysql";
+$dbname = "andre";
 
 // Crear la conexión
 $conexion = mysqli_connect($servidor, $usuario, $password, $dbname);
